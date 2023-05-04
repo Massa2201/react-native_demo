598be442989f7a14e4f596fbb2fde2a7178ddaad
Imai Masashi <chrono.trigger.2424@gmail.com>
2023-05-05 01:44:53 +0900

    チャット一覧画面のコンポーネント

---
+++ src/components/ChatListItem.js
@@ -0,0 +1,59 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+
+const ChatListItem = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+        }}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Text style={styles.name} numberOfLines={1}>
+            Lucas
+          </Text>
+          <Text style={styles.subTitle}>8:38</Text>
+        </View>
+
+        <Text numberOfLines={2} style={styles.subTitle}>
+          Hello There
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "lightgray",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  name: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+  subTitle: {
+    color: "gray",
+  },
+});
+
+export default ChatListItem;
