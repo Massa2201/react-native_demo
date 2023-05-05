11eb1b977690b4269197e8c38a2a8d66e73d5145
Imai Masashi <chrono.trigger.2424@gmail.com>
2023-05-06 00:41:11 +0900

    微々たる修正

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import ChatScreen from "./src/screens/ChatScreen";
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatScreen />
+      <ChatsScreen />
 
       <StatusBar style="auto" />
     </View>
